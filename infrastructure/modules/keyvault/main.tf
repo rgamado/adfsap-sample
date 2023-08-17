d3e5835aca9a7c2749045f083943f72d7701f35f
rgamado <rgamado@example.com>
2023-08-17 15:41:21 +0000

    Adicionado permissões de criar secrets no kv

--- infrastructure/modules/keyvault/main.tf
+++ infrastructure/modules/keyvault/main.tf
@@ -37,6 +37,14 @@ resource "azurerm_key_vault_access_policy" "client" {
   object_id    = data.azurerm_client_config.current.object_id
   tenant_id    = data.azurerm_client_config.current.tenant_id
 
+  secret_permissions = [
+     "Get",
+     "List",
+     "Set",
+     "Delete",
+     "Recover"
+  ]
+
   key_permissions = [
     "Get",
     "List",
